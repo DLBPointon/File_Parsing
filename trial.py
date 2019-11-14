3026a57194f8583f3453667e035bf83b808cd27a
DLBPointon <damonlbp@hotmail.co.uk>
2019-11-14 11:42:02 +0000

    Arg.parse command structure completely re-structured, __doc__ made much more in-depth and now has examples, Surgical Function now saved output

--- trial.py
+++ trial.py
@@ -1,74 +1,265 @@
 """
+-------------------------------------------------------------
+			File Parsing
+-------------------------------------------------------------
 This is my program to take an input file and produce outputs
 based on the function requested by the User.
+-------------------------------------------------------------
+FI - FileInput & FO - FileOutPut are mandatory arguments.
+------------A '.fa' file will be returned!!!-----------------
+-------------------------------------------------------------
+Entry Function is called when -FI, -FO and -en are entered.
+  -  This will split a file into a -en defined number of 
+     enteries per proceduraly generated files.
+     Entries are header and sequence pairs.
+  -  Example Input:
+     'python trial.py -FI ~Skynet/Desktop/FileOfInterest 
+     -FO ~Skynet/Desktop/SaveDir -en 100'
+-------------------------------------------------------------
+Chunks Function is called when -FI, -FO and -ch are entered.
+  -  This will split a file into -ch defined number of base
+     pairs per proceduraly generated file.
+  -  Example Input:
+     'python trial.py -FI ~Replicator/Desktop/FileOfInterest 
+     -FO ~Replicator/Desktop/SaveDir -org Asuran -ch 100000'
+-------------------------------------------------------------
+Surgical Function is called when -FI, -FO, -sc and -ec are 
+     entered.
+  -  This will return the bp between the two user defined 
+     co-ordinates, -sc and -ec (start and end respectively).
+     A file will be produced.
+  -  Example Input:
+
+     python trial.py -FI ~R2D2/Desktop/FileOfInterest 
+     -FO ~R2D2/Desktop/SaveDir -sc 100000 -ec 150000'
+-------------------------------------------------------------
+Joiner Function is called when only -FI and -FO are entered.
+  -  This will concatenate a directories worth of files into
+     a singular file.
+     For this function -FI should end at the directory of 
+     intended use rather at a file of use.
+  -  Example Input:
+
+     'python trial.py -FI ~Hal9000/Desktop/DirOfInterest 
+     -FO ~Hal9000/Desktop/SaveDir'
+-------------------------------------------------------------
+ALL FUNCTIONS
+  -  When you need everything done and now. This will require
+     all args in a specific order:
+  -  Order:
+     -FI -FO -en -org -ch -sc -ec
+  -  Example Input:
+
+     'python trial.py -FI ~Ultron/Desktop/DirOfInterest 
+     -FO ~Ultron/Desktop/SaveDir -en 100 -org Tony -ch 100000
+     -sc 125000 -ec 150000'
+-------------------------------------------------------------
+FILE Nomenclature - Uses the examples above
+  -  Entry - Files returned as: 
+       1.fa, 2.fa, 3.fa, 4.fa, 5.fa... etc
+  -  Chunks - Files returned as: 
+       Asuran|1.fa, Asuran|2.fa, Asuran|3.fa
+  -  Surgical - Files returned as:
+       snipped|100000/150000.fa
+  -  Joiner - Files returned as:
+
+-------------------------------------------------------------
+		   By Damon-Lee Pointon
 """
 import os
 import sys
 import argparse
+import textwrap
 
-
-def parse_command_args():
+def parse_command_args(args=sys.argv[1:]):
 	"""
 	Sets up and parses command line arguments
 
 	Returns:
 		the arguments namespace
 	"""
-	parser = argparse.ArgumentParser(prog='FileParing',
-							description=__doc__)
-
-		#Positional Arguments
-	parser.add_argument('FILEINPUT', 
+	parser = argparse.ArgumentParser(prog='FileParsing',
+							formatter_class = argparse.RawDescriptionHelpFormatter,
+							description = textwrap.dedent(__doc__))
+	parser.add_argument('-v', '--version', 
+							action = 'version', 
+							version = '%(prog)s Beta 3.0')
+	parser.add_argument('-FI', '--FileInput',
+							type=str,
 							action='store',
-							help='Input FASTA file')
-	parser.add_argument('FILEOUT',
+							help='Input FASTA file',
+							dest="FI",
+							required=True)
+	parser.add_argument('-FO', '--FileOutPut',
+							type=str,
 							action='store',
-							help='Output Directory')
-
-		#Optionals
-	parser.add_argument('-org', '-OrganismOI', 
+							help='Output Directory',
+							dest="FO",
+							required=True)
+	parser.add_argument('-ch', '--ChunkSize', 
 							action='store',
 							type=str,
-							help='The Organism of Interest')
-	parser.add_argument('-ch', '-ChunkSize', 
-							action='store',
+							help='The size, in base pairs, that each file is required to be',
+							dest='CS')
+	parser.add_argument('-org', '--OrgansimOI',
 							type=str,
-							help='The size, in base pairs, that each file is required to be')
-	parser.add_argument('-sc', '-StartCoord', 
+							action='store',
+							help='A User defined pre-fix for the save files',
+							dest='ORG')
+	parser.add_argument('-sc', '--StartCoord', 
 							action = 'store',
 							type = int,
-							help = 'The User defined Starting Index for a substring')
-	parser.add_argument('-ec', '-EndCoord', 
+							help = 'The User defined Starting Index for a substring',
+							dest='SC')
+	parser.add_argument('-ec', '--EndCoord', 
 							action = 'store',
 							type = int,
-							help = 'The User defined End index for a substring')
-	parser.add_argument('-En', '-EntryNo', 
+							help = 'The User defined End index for a substring',
+							dest='EC')
+	parser.add_argument('-en', '--EntryNo', 
 							action='store',
 							type=int,
-							help='The number of entries required per file')
-	args = parser.parse_args()
-	options = parser.parse_args(args)
-	return args
-
-args = parse_command_args()
-
-print(args.FILEINPUT)
+							help='The number of entries required per file',
+							dest='EN')
+	options=parser.parse_args(args)
+	return options
 
 def main():
-	options = getOptions(sys.argv[1:])
+	"""
+	A for loop for argument checking and
+	function calling
+	"""
+	options=parse_command_args()
+	for x in sys.argv:
+		print(x)
 
-	try:
-		FI = Options.FILEINPUT
-		FO = Options.FILEOUT
-		CS = Options.ChunkSize
-		EntryNo = Options.EntryNo
-		StartCoord = Options.StartCoord
-		EndCoord = Options.EndCoord
-		OrganismOI = Options.OrganismOI
-		Dir = Options.Directory
-	except NameError:
-		print('Not Found')
+		if x == '-FI' and '-FO':
+
+			if x == '-en':
+				Entry(sys.argv[2], sys.argv[4], int(sys.argv[6]))
+				if not len(sys.argv[1:]) == 6:
+					print('Check your number of arguments, somethings not right')
+					sys.exit(0)
+
+			if x == '-ch':
+				Chunks(sys.argv[2], sys.argv[4], sys.argv[6], int(sys.argv[8]))
+				if not len(sys.argv[1:]) == 8:
+					print('Check your number of arguments, somethings not right')
+					sys.exit(0)
+
+			if x == '-sc':
+				Surgical(sys.argv[2], sys.argv[4], int(sys.argv[6]), int(sys.argv[8]))
+				if not len(sys.argv[1:]) == 8:
+					print('Check your number of arguments, somethings not right')
+					sys.exit(0)
+
+			if x != '-en' or '-ch' or '-sc':
+				Joiner(sys.argv[2], sys.argv[4])
+				if not len(sys.argv[1:]) == 4:
+					print('Check your number of arguments, somethings not right')
+					sys.exit(0)
 
 
+			if x == '-en' and '-org', '-ch' and '-sc' and '-ec': #Cannot be checked for functionality until all funcs work
+				Entry(sys.argv[2], sys.argv[4], int(sys.argv[6]))
+				Chunks(sys.argv[2], sys.argv[4], sys.argv[8], int(sys.argv[10]))
+				Surgical(sys.argv[2], sys.argv[4], int(sys.argv[12]), int(sys.argv[14]))
+				Joiner(sys.argv[2], sys.argv[4])
+				if not len(sys.argv[1:]) == 14:
+					print('Check your number of arguments, somethings not right. Remember, calling all arguments requires a specific order')
+					sys.exit(0)
+
+			else:
+				print('Have you used the correct args?')
+				print(x)
+				sys.exit(0)
+		else:
+			print('You have not used the mandatory -FI (FileInput) and -FO (FileOutPut) flags')
+			print(x)
+			sys.exit(0)
 
 
+def read_fasta(fp): # Temp Until Entry is optimised
+        name, seq = None, []
+        for line in fp:
+            line = line.rstrip()
+            if line.startswith(">"):
+                if name: yield (name, ''.join(seq))
+                name, seq = line, []           
+            else:
+                seq.append(line)
+        if name: yield (name, ''.join(seq))
+			
+def Entry(FI, FO, EN = 1): #Optimise me please
+	
+	Count = 0
+	FileCounter = 0
+	NandS = []
+	
+	with open(FI) as fp:
+		for name, seq in read_fasta(fp):
+			NameSeq = name, seq
+			NandS.append(str(NameSeq).strip())
+			Count += 1 
+			
+			if Count == EN:
+				FileCounter += 1
+				
+				with open(FO + str(FileCounter) + '.fa', 'w') as Done:
+					Done.write(str(NandS).strip('[](),""'''))
+					Count = 0
+					NandS = []
+		
+		else:
+			
+			with open(FO + str(FileCounter) + '.fa', 'w') as Done:
+				Done.write(str(NandS).strip('[](),""''').replace(',', '\n'))
+				NandS = [] #Will be replaced with something waaay better# But Works
+
+#def Chunks(FI, FO, ORG = 'Chunk', CS):
+	#     with open(FI, 'r') as File:
+	#     	Read = File.readline()
+	#     	ToWrite = []
+	#     	Length = 0
+	#     	Counter = 0
+
+	#     	while Read:
+	#         	ToWrite.append(Read)
+	#        	 Length += len(Read)-1
+	#         if Length > CS:
+	#             with open(FO + ORG + '|{}'.format(Counter) + '.fa', 'w') as o:
+	#                 o.write(''.join(ToWrite))
+	#                 Counter += 1
+	#                 ToWrite = []
+	#                 Length = 0
+	#         Read = File.readline()
+	#     with open(FO + ORG + '|{}'.format(Counter) + '.fa', 'w') as o:
+	#         o.write(''.join(ToWrite))# Currently causes errors due to defined names
+
+def Surgical(FI, FO, SC, EC):
+	with open(FI, 'r') as Open:
+		OR = Open.read()
+		OR2 = OR.strip()
+		Find = OR2[SC:EC]
+		with open(f'{FO}snipped|{SC}:{EC}.fa', 'w') as Snipped:
+			print(Find + f'{FO}snipped|{SC}:{EC}.fa', 'w')
+			Snipped.write(Find)#Works
+
+
+def Joiner(FI, FO): #Not Working
+	Total = []
+	Counter = 0
+
+	for file in os.listdir(FO):
+		if file.endswith('.fasta' or '.fa' or '.fna'):
+			fullpath = os.path.join(FO, file)
+			Total.append(fullpath)
+			with open(FO + Counter, 'w') as outfile:
+				for filename in Total:
+					with open(filename ,'r') as infile:
+						for line in infile:
+							outfile.write(line)
+
+
+if __name__ == '__main__':
+	main()
